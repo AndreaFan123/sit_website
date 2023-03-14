406c1af08a12fae18f801a46b56ad0161f9c7981
andrea.fan <andrea.fan@ringus-solution.com>
2023-03-14 20:59:15 +0800

    Update: Implement navbar active status and main section bg and texts

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,56 @@
+<script>
+  import arrowDown from "../lib/assets/icons/arrow_down.svg";
+</script>
+<main>
+  <div class="hero"></div>
+  <div class="hero-container">
+    <h1>Diagnostics</h1>
+    <p>The most important part of maintaining Engines, Machines and Production-Lines. For fast rectification of the error and damage, it is of advantage to have enough experienced technicians.</p>
+    <p>Human knowledge is needed. Industry 5.0!</p>
+  </div>
+  <div>
+    <img src={arrowDown} alt="arrow down"/>
+  </div>
+</main>
+
+<style lang="scss">
+  .hero {
+    width: 100%;
+    min-height: 100vh;
+    background-image: url("../lib//assets/landing/landing-bg.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    filter:brightness(25%);
+    position: absolute;
+    z-index: 0;
+  }
+
+  .hero-container {
+    position: relative;
+    z-index: 100;
+    text-align: center;
+    margin: 0 auto;
+    max-width: 800px;
+
+    h1 {
+      color: var(--brand-primary-red);
+      font-size: 5rem;
+      padding-top: 9rem;
+      padding-bottom: 2rem;
+    }
+
+    p {
+      color: var(--bright2);
+      font-size: 1.7rem;
+      padding: 0 1rem;
+    }
+
+    :last-child {
+      color: var(--brand-primary-red);
+      font-weight: 800;
+    }
+  }
+
+
+</style>
--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,51 +1,147 @@
 <script>
   import sit_logo from '$lib/assets/sit_logo.png';
+  import { page } from '$app/stores';
+  
+  $: routeId = $page.route.id;
+
+  const navLists = [
+    {
+      routeName: "Home",
+      url: "/"
+    },
+    {
+      routeName: "Product",
+      url: "/product"
+    },
+    {
+      routeName: "News",
+      url: "/news"
+    },
+    {
+      routeName: "Team",
+      url: "/team"
+    },
+    {
+      routeName: "Contact",
+      url: "/contact"
+    },
+  ]
 </script>
 
-<nav>
-  <div>
-    <a href="/">喜德科技有限公司 | S.I.T. Technology</a>
+<header class="header-wrapper" role="navigation">
+  <div class="header-container">
+    <div class="header-logoText">
+      <a href="/">喜德科技有限公司 | S.I.T. Technology</a>
+    </div>
+    <div class="header-logoImg">
+      <img src={sit_logo} alt="S.I.T Tech" />
+    </div>
+    <nav class="header-nav-container" aria-label="desktop-navigation">
+      <ul>
+        {#each navLists as { routeName, url }}
+          <li>
+            <a href={url} class:active={routeId === url} title={routeName}>{routeName}</a>
+          </li>
+        {/each}
+      </ul>
+    </nav>
   </div>
-  <div>
-    <img src={sit_logo} alt="S.I.T Tech" />
-  </div>
-  <ul>
-    <li><a href="/product">Product</a></li>
-    <li><a href="/news">News</a></li>
-    <li><a href="/team">Team</a></li>
-    <li><a href="/contact">Contact</a></li>
-  </ul>
-</nav>
+</header>
 
 <style lang="scss">
-  nav {
-    width: 100%;
-    /* background-color: azure; */
-    color: var(--bright1);
+
+.header-wrapper {
+  z-index: 100;
+  margin: 0 auto;
+  right: 0;
+  left: 0;
+  position: relative;
+  min-height: 60px;
+  border-bottom: 2px solid var(--brand-primary-red);;
+
+  .header-container {
+    margin: 0 auto;
+    /* padding: 1rem 2rem; */
+    height: 80px;
     display: flex;
-    justify-content: space-evenly;
+    justify-content: space-between;
     align-items: center;
-    padding: 1rem;
 
-    ul {
-      display: flex;
-      justify-content: space-evenly;
-      li {
-        padding: 0 1.5rem;
+    /* Left: logo-text-link */
+    .header-logoText {
+      display: inline-block;
 
-        a {
-          text-decoration: none;
-          &:hover{
-            transition: all 0.3s ease-in-out;
-            color: var(--brand-primary-red);
-            cursor: pointer;
-          }
-          &:focus {
-            outline: 2px dashed var(--brand-primary-red);
-            padding: .2rem;
-          }
+      a {
+        display: inline-block;
+        &:hover {
+          transition: all .3s ease-in-out;
+          color: var(--brand-primary-red);
         }
       }
     }
+    /* Media Query */
+    @media screen and (min-width: 1200px) {
+      max-width: 1170px;
+    }
+
+    @media screen and (min-width: 992px) {
+      height: 80px;
+    }
   }
+  
+  /* Main-nav */
+  .header-nav-container {
+    ul {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      gap: 1rem;
+
+    li {
+      display: inline-block;
+      
+      a {
+        display: inline-block;
+        position: relative;
+        font-weight: 800;
+        padding: 5px 10px;  
+
+      &.active {
+        color:var(--brand-primary-red);
+      }  
+      &:hover {
+        color:var(--brand-primary-red);
+        transition: all 0.3s ease-in 0s;
+      }
+
+      &:focus {
+        outline: none;
+        box-shadow: 0 0 0 3px var(--brand-primary-red);
+      }
+	    &:after {
+          content: "";
+          display: block;
+          position: absolute;
+          width: 0;
+          bottom: 0;
+          height: 2px;
+          margin: -5px 0;
+          left: 0;
+          background-color: var(--brand-primary-red);
+          transition: all 0.3s ease-in 0s;
+        }
+          &:hover:after {
+            width: 100%;
+          }
+}
+      }
+
+      /* Media Query */
+      @media screen and (min-width: 992px)  {
+        display: inline-flex;
+      }
+    }
+  }
+}
+
 </style>
